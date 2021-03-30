7f27a5addc154e15d8026ac96d17dcf77c53aec0
TayIsAsleep <57579136+TayIsAsleep@users.noreply.github.com>
2021-03-30 14:12:00 +0200

    Added some options to QR code

--- static/js/qr.js
+++ static/js/qr.js
@@ -5,17 +5,28 @@ else{
     var id = passedID;
 }
 
-function htmlEncode(value) { 
-  return $('<div/>').text(value).html(); 
+if (id == null){
+    id = prompt("Skriv in ditt ID:");
+    a_or_id = "?id=";
 }
 
+function htmlEncode(value) { 
+    return $('<div/>').text(value).html(); 
+}
 var url = requestURL + a_or_id + id;
 
-$('#maintext').text(id);
+
+if (privateID){
+    $("#maintext").text(prompt("Skriv in namn på schemat:"));
+}
+else{
+    $('#maintext').text(id);
+}
+
 $('#subtext').text(url);
 
-var witdh = $('#qr-code').width();
-var height = $('#qr-code').height()
+//var witdh = $('#qr-code').width();
+//var height = $('#qr-code').height()
 
 let finalURL = 'https://chart.googleapis.com/chart?cht=qr&chl=' + htmlEncode(url) + '&chs=160x160&chld=L|0' 
 
