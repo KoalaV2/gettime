0dc15b2f0d05864e6a9a7b120c2c70282db4bc7f
TayIsAsleep <57579136+TayIsAsleep@users.noreply.github.com>
2021-04-20 19:25:31 +0200

    Ignoring input if on mobile

--- static/js/script.js
+++ static/js/script.js
@@ -681,24 +681,25 @@ $(window).on("load", function(){
 
 	//Code from https://stackoverflow.com/a/1846733
 	document.onkeypress = function(evt) {
-		evt = evt || window.event;
-		var charCode = evt.keyCode || evt.which;
-		var charStr = String.fromCharCode(charCode);
-
-		if (charStr.toLowerCase() == "f"){
-			if (hideNavbar){
-				f_showNavbar();
-			}
-			else{
-				f_hideNavbar();
+		if (mobileRequest || document.activeElement == document.getElementById('id-input-box') || document.activeElement == document.getElementById('id-input-box2')){
+			return;
+		}
+		else
+		{
+			evt = evt || window.event;
+			var charCode = evt.keyCode || evt.which;
+			var charStr = String.fromCharCode(charCode);
+	
+			if (charStr.toLowerCase() == "f"){
+				if (hideNavbar){
+					f_showNavbar();
+				}
+				else{
+					f_hideNavbar();
+				}
 			}
 		}
 	};
-
-	// Moves the timetable down so it doesnt overlay the navbar
-	//$(document).ready(function() {
-	//	$("#scheduleBox").css("top", $(".navbar").height());
-	//});
 
 	if (showContactOnLoad){
 		textBoxOpen('#text_contact_info');
