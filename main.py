6237e531b9564f32bec927e188875d92dceba647
TayIsAsleep <57579136+TayIsAsleep@users.noreply.github.com>
2021-08-31 12:53:27 +0200

    GTM.1.0.2.1 BETA

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-version = "GTM.1.0.2 BETA"
+version = "GTM.1.0.2.1 BETA"
 #region ASCII ART
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #               _   _   _                    __            _          _                             #
