cd5d4395e699239c3a7fce4fb8f3dedf0332796a
KoalaV2 <theo.johanson@gmail.com>
2021-11-29 10:02:58 +0100

    fix version number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-version = "GTM.1.0.2.5 BETA"
+version = "GTM.1.0.3"
 #region ASCII ART
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #               _   _   _                    __            _          _                             #
