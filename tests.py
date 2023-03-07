917421e0e7f496b69b75f40531be044821a65eda
KoalaV2 <theo.johanson@gmail.com>
2023-03-07 20:10:50 +0100

    Use pytest instead of unittest

--- tests.py
+++ tests.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python3
 
-import unittest
 import requests
 
-class Test(unittest.TestCase):
-    def test_mainpage(self):
-        r = requests.get('http://0.0.0.0:7331/')
-        self.assertEqual(r.status_code, 200)
-    def test_apistatus(self):
-        r = requests.get('http://0.0.0.0:7331/API/HEALTH')
-        self.assertEqual(r.text, "OK")
-    def test_jsonapi(self):
-        params = {
-                "school": "IT-Gymnasiet Södertörn",
-                "id": "20el2",
-                "day": 0
-                }
-        r = requests.get('http://0.0.0.0:7331/API/JSON',params).json()
-        self.assertEqual(r['status'], 0)
-        self.assertEqual(r['message'], "OK")
-    def test_foodapi(self):
-        params = {"school": "Surteskolan"}
-        r = requests.get('http://0.0.0.0:7331/API/FOOD_REDIRECT',params)
-        self.assertEqual(r.url, "https://skolmaten.se/surteskolan/")
-    def test_privatelink(self):
-        params = {"school": "IT-Gymnasiet Södertörn", "id": "20el2"}
-        r = requests.get('http://0.0.0.0:7331/API/SHAREABLE_URL',params)
-        self.assertEqual(r.status_code, 200)
-        self.assertIsNotNone(r.text)
+def test_mainpage():
+    r = requests.get('http://0.0.0.0:7331/')
+    assert r.status_code == 200
+def test_apistatus():
+    r = requests.get('http://0.0.0.0:7331/API/HEALTH')
+    assert r.text ==  "OK"
+def test_jsonapi():
+    params = {
+            "school": "IT-Gymnasiet Södertörn",
+            "id": "20el2",
+            "day": 0
+            }
+    r = requests.get('http://0.0.0.0:7331/API/JSON',params).json()
+    assert r['message'] == "OK"
+    assert r['status'] ==  0
 
-if __name__ == '__main__':
-    unittest.main()
+def test_foodapi():
+    params = {"school": "Surteskolan"}
+    r = requests.get('http://0.0.0.0:7331/API/FOOD_REDIRECT',params)
+    assert r.url == "https://skolmaten.se/surteskolan/"
+def test_privatelink():
+    params = {"school": "IT-Gymnasiet Södertörn", "id": "20el2"}
+    r = requests.get('http://0.0.0.0:7331/API/SHAREABLE_URL',params)
+    assert r.status_code == 200
+    assert r.text != None
